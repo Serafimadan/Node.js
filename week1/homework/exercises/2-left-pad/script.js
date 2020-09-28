aa549d9fc3018597a276f48f06a441ee20cbd032
serafima <svp1982@mail.ru>
2020-09-28 19:38:06 +0200

    Second exercise

--- week1/homework/exercises/2-left-pad/script.js
+++ week1/homework/exercises/2-left-pad/script.js
@@ -11,3 +11,9 @@
 let numbers = [ "12", "846", "2", "1236" ];
 
 // YOUR CODE GOES HERE
+const leftPad = require( 'left-pad' );
+
+console.log(leftPad(numbers[0], 8, '_'));
+console.log(leftPad(numbers[1], 8, '_'));
+console.log(leftPad(numbers[2], 8, '_'));
+console.log(leftPad(numbers[3], 8, '_'));
